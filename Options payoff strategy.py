f9b51d8deb63caeb65a3413bb5d20413c10e05b9
Milani Milani  Chikeka <milaninnamdi@gmail.com>
2024-11-21 08:41:26 +0200

    Addedd the last few labels and buttons

--- Options payoff strategy.py
+++ Options payoff strategy.py
@@ -70,3 +70,31 @@ strike_prices_entry.grid(row=0, column=1, padx=10, pady=5)
 Label(root,text="Premiums (space-seperated):").grid(row=1, column=0, sticky="w", padx=10, pady=5)
 premiums_Entry=Entry(root,width=40)
 premiums_Entry.grid(row=1, column=1, padx=10, pady=5)
+
+Label(root, text="Option Type (space-seperated):").grid(row=2, column=0, sticky="w", padx=10, pady=5)
+option_type_var=StringVar(root)
+option_type_var.set("Call")# This is the default option type
+OptionMenu(root,option_type_var, "Call", "Put", "Short_Call", "Short_Put").grid(row=2, column=0, sticky="w", padx=10, pady=5)
+
+#The sliders for the spot price ranges
+Label(root, text="Spot price start:").grid(row=3, column=0, sticky="w", padx=10, pady=5)
+Spot_start_Slider=Scale(root,from_=0, to=200, orient=Horizontal, length=300)
+Spot_start_Slider.set(50)
+Spot_start_Slider.grid(row=3, column=1, padx=10, pady=5)
+
+Label(root, text="Spot price End:").grid(row=4, column=0, sticky="w", padx=10, pady=5)
+Spot_end_slider=Scale(root, from_=0, to=200, orient=Horizontal,length=300)
+Spot_end_slider.set(150)
+Spot_end_slider.grid(row=4, column=1, padx=10, pady=5)
+
+Label(root,text="Spot price step:").grid(row=5, column=0, padx=10, pady=5)
+spot_step_slider=Scale(root, from_=1, to=20, oreint=Horizontal, length=300)
+spot_step_slider.set(5)
+spot_step_slider.grid(row=5, column=1, padx=10, pady=5)
+
+#The buttons for saving and plotting
+Button(root, text="Plot Payoff diagram", command=lambda: calculate_and_plot(save=False)).grid(row=6, column=0, padx=10, pady=10)
+Button(root, text="Save payoff diagram", command=lambda: calculate_and_plot(save=True)).grid(row=6, column=0, padx=10, pady=10)
+
+#Run the application
+root.mainloop()
