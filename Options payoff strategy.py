ab7c0ec65914b9ebd8e87f065058c3c00fd3d72b
Milani Milani  Chikeka <milaninnamdi@gmail.com>
2025-01-20 10:22:58 +0200

    Made synax fixes

--- Options payoff strategy.py
+++ Options payoff strategy.py
@@ -12,7 +12,7 @@ def calculate_and_plot(save=False):
         premiums_Entry=np.array(list(map(float,premiums_Entry.get().split())))
         
         if len(strike_prices_entry) !=len(premiums_Entry):
-            raise ValueError("Strike prices and premiums must have the saem number of entries")
+            raise ValueError("Strike prices and premiums must have the same number of entries.")
         
         spot_start_slider=spot_start_slider.get()
         spot_end_slider=spot_end_slider.get()
